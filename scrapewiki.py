5cddc9c9266f7e14d2b7cdab2e404e6f8468c918
ricsrdocasro <rcastropaula02@gmail.com>
2021-05-28 21:05:42 -0300

    Update scrapewiki.py to receive different urls and scrape them well, retrieve and save images

--- scrapewiki.py
+++ scrapewiki.py
@@ -7,6 +7,8 @@ from bs4 import *
 import pandas as pd
 import html.parser
 import re
+from PIL import Image
+import requests
 
 driver = webdriver.Chrome(executable_path='C:\Python39\Scripts\chromedriver')
 
@@ -15,47 +17,73 @@ descriptionList=[] #List to store price of the product
 categoryList=[]
 imagesList=[] #List to store rating of the product
 
-
-driver.get('https://en.wikipedia.org/wiki/Huilliche_people')
+link = input("Insira o link da wikipédia: \n")
+if "https://" in link:
+    lang=link[8:10]
+    print(lang)
+    
+else:
+    lang=link[1:]
+    print(lang)
+    
+driver.get(link)
 content = driver.page_source
 soup = BeautifulSoup(content, 'html.parser')
 
-name=soup.find('h1', attrs={'id':'firstHeading'})
-image=soup.find('img', attrs={'class':'pi-image-thumbnail'})
+name = soup.find('h1', attrs={'id':'firstHeading'})
+name = name.text
 
 for a in soup.findAll('div', attrs={'class':'mw-parser-output'}):
-    description=''
-    a = a.find('p')
-    description+=a.get_text()
-    break
-    
-print(description)
-    
-descriptionList.append(description)    
-namesList.append(name.text)
+        description=''
+        a = a.find('p')
+        description+=a.get_text()
+        break
 
-driver.get('https://pageviews.toolforge.org/?project=en.wikipedia.org&platform=all-access&agent=user&redirects=0&range=latest-20&pages=Huilliche_people')
+try:
+    for a in soup.findAll('td', attrs={'class':'infobox-image'}):
+        image=a.find('img')
+        image=image['src'][2:]
+        print(image)
+        break
+
+    imagesList.append(image)
+
+except:
+    for a in soup.findAll('div', attrs={'class':'thumbinner', 'style':'width:262px;'}):
+        image=a.find('img')
+        image=image['src'][2:]
+        print(image)
+        break
+        
+    imagesList.append(image)
+
+imageLink = 'http://' + image
+imageAsFile = Image.open(requests.get(imageLink, stream=True).raw)
+imageAsFile.save('wiki.png')
+    
+print(name)
+print(description)
+
+descriptionList.append(description)    
+namesList.append(name)
+
+driver.get('https://pageviews.toolforge.org/?project=%s.wikipedia.org&platform=all-access&agent=user&redirects=0&range=latest-20&pages=%s' %(lang, name.replace(" ", "_")))
 
 element = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CLASS_NAME, "legend-block")))
 
-print(element)
 content2 = element.get_attribute('innerHTML')
-print(content2)
 soup2 = BeautifulSoup(content2, 'html.parser')
 views = []
 
 for a in soup2.find_all('span', attrs={'class':'pull-right'}):
     views.append(a.get_text())
-    print(views)
     
 viewsTotal = views[0]
 viewsPerDay = views[1]
 viewsTotal = re.sub(r"[\n\t\s]*", "", viewsTotal)
 viewsPerDay = re.sub(r"[\n\t\s]*", "", viewsPerDay)
-print(viewsTotal)
-print(viewsPerDay)
 
-data = {'Name': namesList, 'Description': descriptionList, 'Total views': viewsTotal, 'Average views per pay': viewsPerDay}
+data = {'Image': imagesList, 'Name': namesList, 'Description': descriptionList, 'Total views': viewsTotal, 'Average views per pay': viewsPerDay}
 
 df = pd.DataFrame(data=data)
 df.to_excel("all.xlsx")
